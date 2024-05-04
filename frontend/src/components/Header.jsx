3498ee4ae284cf0536d85b1da6829adf04729839
Sayan Banerjee <sayankb.001@gmail.com>
2024-05-05 01:15:41 +0530

    Filtering course for small screen added and navbar search toggle is browser no browser compatible.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,6 +9,7 @@ const Header = () => {
   const [searchText, setSearchText] = useState('')
   const allCourses = useSelector(state => state.courses.allCourses)
   const dispatch = useDispatch()
+  const [hidden, setHidden] = useState(true)
 
   const navItems = [
     {
@@ -61,10 +62,8 @@ const Header = () => {
         <div className="flex md:order-2">
           <button
             type="button"
-            data-collapse-toggle="navbar-search"
-            aria-controls="navbar-search"
-            aria-expanded="false"
             className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
+            onClick={() => setHidden(prev => !prev)}
           >
             <svg
               className="w-5 h-5"
@@ -112,11 +111,9 @@ const Header = () => {
             />
           </div>
           <button
-            data-collapse-toggle="navbar-search"
             type="button"
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-search"
-            aria-expanded="false"
+            onClick={() => setHidden(prev => !prev)}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -137,7 +134,9 @@ const Header = () => {
           </button>
         </div>
         <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          className={`items-center justify-between ${
+            hidden ? 'hidden' : ''
+          } w-full md:flex md:w-auto md:order-1`}
           id="navbar-search"
         >
           <div className="relative mt-3 md:hidden">
@@ -163,6 +162,8 @@ const Header = () => {
               id="search-navbar"
               className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Search..."
+              value={searchText}
+              onInput={searchHandler}
             />
           </div>
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
